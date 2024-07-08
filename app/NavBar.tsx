98473d82bd3fbfddedb4dc57a675f1c6e23a0755
Richard Dalrymple <117475164+Tcherin@users.noreply.github.com>
2024-07-08 21:55:35 +0100

    add the login and logout links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,9 +4,12 @@ import Link from "next/link";
 import { FaBug } from "react-icons/fa";
 import { usePathname } from "next/navigation";
 import classNames from "classnames";
+import { useSession } from "next-auth/react";
+import { Box } from "@radix-ui/themes";
 
 const NavBar = () => {
   const currentPath = usePathname();
+  const { status, data: session } = useSession();
 
   const links = [
     { label: "dashboard", href: "/" },
@@ -20,35 +23,30 @@ const NavBar = () => {
       </Link>
       <ul className="flex space-x-6">
         {links.map((link) => (
-          <Link
-            key={link.href}
-            className={classNames({
-              "text-zinc-900": link.href === currentPath,
-              "text-zinc-500": link.href !== currentPath,
-              "hover:text-zinc-800 transition-colors": true,
-            })}
-            href={link.href}
-          >
-            {link.label}
-          </Link>
+          <li key={link.href}>
+            <Link
+              className={classNames({
+                "text-zinc-900": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
         ))}
       </ul>
+      <Box>
+        {status === "authenticated" && (
+          <Link href="/api/auth/signout">Log Out</Link>
+        )}
+        {status === "unauthenticated" && (
+          <Link href="/api/auth/signin">Log In</Link>
+        )}
+      </Box>
     </nav>
   );
 };
 
 export default NavBar;
-
-// //  <ul className="flex space-x-6">
-// {links.map((link) => (
-//   <Link
-//     key={link.href}
-//     className={` ${
-//       link.href === currentPath ? "text-zinc-900" : "text-zinc-500"
-//     }  hover:text-zinc-800 transition-colors`}
-//     href={link.href}
-//   >
-//     {link.label}
-//   </Link>
-// ))}
-// </ul>
