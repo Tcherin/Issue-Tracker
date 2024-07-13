f13705e26b0e4c1277b650bb4af0efb526283ac5
Richard Dalrymple <117475164+Tcherin@users.noreply.github.com>
2024-07-13 11:28:56 +0100

    enhance api to assign issues to users

--- app/api/issues/[id]/route.tsx
+++ app/api/issues/[id]/route.tsx
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { issueSchema } from "../validationSchema";
+import { patchIssueSchema } from "../validationSchema";
 import prisma from "@/prisma/client";
 
 export async function DELETE(
@@ -24,9 +24,16 @@ export async function PATCH(
   { params }: { params: { id: string } }
 ) {
   const body = await request.json();
-  const validation = issueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
+
+  const { assignedToUserId, title, description } = body;
+  if (assignedToUserId) {
+    const user = prisma.user.findUnique({ where: { id: assignedToUserId } });
+    if (!user)
+      return NextResponse.json({ error: "Invalid user." }, { status: 400 });
+  }
 
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
@@ -37,8 +44,9 @@ export async function PATCH(
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
     data: {
-      title: body.title,
-      description: body.description,
+      title,
+      description,
+      assignedToUserId,
     },
   });
 
